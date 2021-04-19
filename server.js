ebce3a75ca0cbac7c97c9c7b52b9102691bba15e
Maxime DELOBELLE <maxime.delobelle@niji.fr>
2021-04-19 15:52:16 +0200

    deploy in heroku - dist folder  server error

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+console.log(__dirname)
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fraymapp/APP/'));
+app.use(express.static('./dist/app/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fraymapp/APP/index.html'));
+res.sendFile(path.join('./dist/app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
